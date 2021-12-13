d962f95f13d00ff287dd99c0a8b2c87f84d8a536
Narroya5 <95328541+Narroya5@users.noreply.github.com>
2021-12-12 22:25:40 -0500

    Add files via upload
    se agrega la clase text para que se cargue texto en los botones , y se agrega a los assets fuentes para botones, titulo , subtítulos.

--- src/graphics/Assets.java
+++ src/graphics/Assets.java
@@ -1,6 +1,7 @@
 
 package graphics;
 
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 
 
@@ -20,12 +21,31 @@ public class Assets {
     public static BufferedImage[] smalls = new BufferedImage[2];
     public static BufferedImage[] tinies = new BufferedImage[2];
     
+    public static BufferedImage blueBtn;
+    public static BufferedImage greyBtn;
+    
+    //fuentes 
+    public static Font fontMed;
+    public static Font fontsmall;
+    public static Font fonttitle;
     public static void init() {
         player = Loader.ImageLoader("/ships/player.png");
         speed = Loader.ImageLoader("/effects/fire08.png");
         blueLaser = Loader.ImageLoader("/lasers/laserBlue01.png");
         greenLaser = Loader.ImageLoader("/lasers/laserGreen11.png");
         redLaser = Loader.ImageLoader("/lasers/laserRed01.png");
+        
+        
+        //fuentes titulo menu 
+        fonttitle = Loader.loadFont("/fonts/futureFont.ttf", 42);
+        
+        //fuente botones
+        fontMed = Loader.loadFont("/fonts/futureFont.ttf", 20);
+        //fuente titulos h2
+        fontsmall = Loader.loadFont("/fonts/futureFont.ttf", 16);
+        //imagenes de los botones 
+        greyBtn = Loader.ImageLoader("/ui/grey_button.png");
+        blueBtn = Loader.ImageLoader("/ui/blue_button.png");
         
         for(int i = 0; i < bigs.length ; i++)
             bigs[i] = Loader.ImageLoader("/meteors/big"+(i+1)+".png");
--- src/graphics/Loader.java
+++ src/graphics/Loader.java
@@ -1,7 +1,10 @@
 
 package graphics;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 
 
@@ -15,4 +18,16 @@ public class Loader {
         }
         return null;
     }
+    
+   public static Font loadFont(String path, int size) {
+			try {
+				return Font.createFont(Font.TRUETYPE_FONT, Loader.class.getResourceAsStream(path)).deriveFont(Font.PLAIN, size);
+			} catch (FontFormatException | IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+	}
+    
+    
+    
 }
