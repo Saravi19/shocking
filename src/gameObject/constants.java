19701302fa0a5b1ee35c2bca379fd322d6de7246
Narroya5 <95328541+Narroya5@users.noreply.github.com>
2021-12-12 22:22:38 -0500

    Add files via upload
    se agregan constantes para el apartado instrucciones, botones y titulo del menu.

--- src/gameObject/constants.java
+++ src/gameObject/constants.java
@@ -19,4 +19,29 @@ public class constants {
     //propiedades de meteoro
     public static final double METEOR_VEL = 2.0;
     
+    
+    //string del titulo menu
+    
+        public static final String part1 = "Shocking";
+        public static final String part2 = "col";
+        public static final String part3 ="or";
+        public static final String part4 ="ful";
+         public static final String part5 ="meteors";
+    
+    
+    
+      //string de los botones 
+        public static final String PLAY = "PLAY";
+	public static final String EXIT = "SALIR";
+        public static final String RETURN = "REGRESAR";
+        public static final String INSTRUCTION = "INSTRUCCIONES";
+        
+        
+        /// string de las instrucciones
+        public static final String LINEA1 = "1.Debes Manejar y disparar a los crateres antes de terminar el tiempo";
+        public static final String LINEA2 = "2.En la parte central de la pantalla aparecera la palabra a la cual debes disparar";
+        public static final String LINEA3 ="3.la palabra estara en idioma ingles";
+        public static final String LINEA4 ="4.Si disparas a la plabara mal exrita perderas una vida; recuerda cuentas con 3!";
+        
+	
 }
